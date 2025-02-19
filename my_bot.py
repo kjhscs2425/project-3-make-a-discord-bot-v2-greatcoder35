d63e570552c8cc03d648a11c0a208b9b8e2a0f27
Jordan Nash <jordangabrielnash@gmail.com>
2025-02-18 17:51:40 -0800

    finished project ready for presentation

--- my_bot.py
+++ my_bot.py
@@ -1,7 +1,9 @@
 from secret import swear_list
 import random
-rps_ans = None
-
+import main
+bot_choice = None
+user_choice = None
+result = None
 """
 **Do NOT change the name of this function.**
 
@@ -12,18 +14,21 @@ This function will be called every time anyone says anything on a channel where 
 * You can have the bot respond differently to different users
 """
 def should_i_respond(user_message, user_name):
-  if "bot" in user_message:
-      return True
-  elif swear_list in user_message:
-      return True
-  elif "calc" in user_message:
-      return True
-  elif "rock paper scissors" in user_message:
-      return True
-  elif "cheater" in user_message:
-      return True
-  else:
-      return False
+    trigger_word = [
+   "bot",
+   "calc",
+   "good at driving",
+   "riddle",
+   "yell this: ",
+   "ping ",
+   "you suck",
+   "your a bad bot",
+   "deebs"
+] + swear_list
+    for word in trigger_word:
+       if word in user_message:
+          return True
+    return False
 
 """
 **Do NOT change the name of this function.**
@@ -38,13 +43,71 @@ def respond(user_message, user_name):
 
   if len(user_message) >= 50:
     return "wow, thats a long message, can you shorten that for me? ;)"
-  elif swear_list in user_message:
+  elif any(swear in user_message for swear in swear_list):
     return "hey thats not very nice. please refrain from using that language"
+  elif "deebs" in user_message:
+     return "deebs me timbers" # inside joke with friends (using this bot in other servers)
   elif "calc" in user_message:
     return "calc is short for calculator"
-  elif "rock paper scissors" in user_message:
-    return f"ok let me think... i pick {random.choice(["rock", "paper", "scissors"])}"
-  elif "cheater" in user_message:
-    return ":shrug:"
+  elif "good at driving" in user_message:
+     if "I" in user_message:
+        return "yes"
+     else:
+        return ":shrug:"
+  elif "riddle" in user_message:
+     return random.choice([
+        "What has to be broken before you can use it? ||An Egg!||",
+        "What has hands but can't clap? ||A Clock!||",
+        "The more you take, the more you leave behind. What am I? ||Footsteps!||",
+        "I'm tall when I'm young and short when I'm old. What am I? ||A Candle||",
+        "I fly without wings, I cry without eyes. What am I? ||A Cloud!||"
+     ])
+  elif "yell this: " in user_message:
+     yelled_message = user_message[11:].upper()
+     return yelled_message
+  elif "ping " in user_message:
+     ptp = (user_message[5:] + " ") * 50
+     return ptp
+  elif "you suck" in user_message:
+     return f' I think you meant to say "{user_message.replace("suck","are the best :star_struck:")}"'
+  elif "your a bad bot" in user_message:
+     return f'I think you meant to say "{user_message.replace("a bad bot","an amazing bot :star_struck:")}"'
   else:
     return f"what's up, {user_message.replace("bot", user_name)}"
+
+
+
+
+
+# attempt to make rock paper scissors (did not work and cant figure it out)
+
+  # for word in rps_command:
+  #   if word in user_message:
+  #     bot_choice = random.choice(["rock", "paper", "scissors"])
+  #     if "rrock" in user_message:
+  #         user_choice = "rock"
+  #     elif "ppaper" in user_message:
+  #       user_choice = "paper"
+  #     elif "sscissors" in user_message:
+  #       user_choice = "scissors"
+  #     elif "rock paper scissors" in user_message:
+  #       "Please add the first letter twice to play (ex. rrock to choose rock)"
+  #     else:
+  #       return "try again :/"
+
+  #     if bot_choice == user_choice:
+  #         result = "I win, haha"
+  #     elif bot_choice == "rock" and user_choice == "paper":
+  #         result = "you win, nice"
+  #     elif bot_choice == "rock" and user_choice == "scissors":
+  #         result = "I win haha"
+  #     elif bot_choice == "paper" and user_choice == "rock":
+  #         result = "I win haha"
+  #     elif bot_choice == "paper" and user_choice == "scissors":
+  #         result = "you win, nice"
+  #     elif bot_choice == "scissors" and user_choice == "rock":
+  #       result = "you win, nice"
+  #     elif bot_choice == "scissors" and user_choice == "paper":
+  #       result = "I win haha"
+
+  #     return f"you chose {user_choice}, and i chose {bot_choice}. {result}"
--- proposal.md
+++ proposal.md
@@ -8,41 +8,18 @@ My bot will respond by: hey thats not very nice. please refrain from using that 
 
 When a user says: calc
 My bot will respond by: is short for calculator
--------------------------------------------------------------------------------------------------------
+
 When a user says: is ___ good at driving
 My bot will respond by: only say yes for some people :D
 
-When a user says: what classes do i have this week? 
-My bot will respond by: list all of my classes in vertical format (line break after each) and how many i have left this week in order of how many i have left from top to bottom
-
-When a user says: ping ___
-My bot will respond by: spam ping whoever i said to ping (send 20 consecutive messages with their @)(i will create variables linked to my friends names with their @'s)
-
 When a user says: tell me a riddle
 My bot will respond by: *gives riddle*
 
-When a user says: play rock paper scissors
-My bot will respond by: playing rock paper scissors with you. add secret move you can play to always win every 3rd round
-
-When a user says: play tic tac toe
-My bot will respond by: play tic tac toe
-
-
-
-When a user says: dewbs
-My bot will respond by: deebs
-
-When a user says: whats the date today
-My bot will respond by: (say the date in both words and numbers)
-
-When a user says: what classes do i have this week? 
-My bot will respond by: list all of my classes in vertical format (line break after each) and how many i have left this week in order of how many i have left from top to bottom
-
 When a user says: ping ___
 My bot will respond by: spam ping whoever i said to ping (send 20 consecutive messages with their @)(i will create variables linked to my friends names with their @'s)
-
-When a user says: tell me a riddle
-My bot will respond by: *gives riddle*
+-------------------------------------------------------------------------------------------------------
+When a user says: what classes do i have this week? 
+My bot will respond by: list all of my classes in vertical format (line break after each) and how many i have left this week in order of how many i have left from top to bottom
 
 When a user says: play rock paper scissors
 My bot will respond by: playing rock paper scissors with you. add secret move you can play to always win every 3rd round
